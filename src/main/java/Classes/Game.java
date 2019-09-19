17a2f306d5eee9dc4083ef0eefac6f677ed7f2cb
80ant <80ant@DESKTOP-8R81VGJ>
2019-09-19 22:18:18 +0200

    implemented all classes and unit tests.

--- src/main/java/Classes/Game.java
+++ src/main/java/Classes/Game.java
@@ -14,35 +14,58 @@ public class Game {
     private Player p1;
     //Stocks informations about player 2
     private Player p2;
+    //Stocks the possible scores
+    private final String[] tabScore = {"0\t","15\t","30\t","40\t","advantage"};   //tabulation added at the end of each number so the "advantage" doesn't look too long
+    //Stocks the score of the first players
+    private int p1Score;
+    //Stocks the score of the second players
+    private int p2Score;
     
     //Default Constructor
-    public Game(String player1, String player2){
-        System.out.println("A game has begun ! Welcome "+player1+" and "+player2);
+    public Game(Player player1, Player player2){
+        System.out.println("A game has begun ! Welcome "+player1.getName()+" and "+player2.getName());
         System.out.println("------------------------------------------------------");
-        this.p1 = new Player(player1);
-        this.p2 = new Player(player2);
+        this.p1 = player1;
+        this.p2 = player2;
+        this.p1Score = 0;
+        this.p2Score = 0;
+    }
+
+    public int getP1Score() {
+        return p1Score;
+    }
+
+    public int getP2Score() {
+        return p2Score;
+    }
+    
+    
+    
+    //Returns player's real score
+    private String getScore(int score){
+        return tabScore[score];
     }
     
     //player 1 scored a point
     public void p1Scored(){
         
-        if ( (p1.scorePos() < 3) || ( p1.scorePos() == 3 && p2.scorePos() == 3 ) )  //If player 1 is below "40", he scores a point. IF player 1 and player 2 are at "40", player 1 gains advantage.
+        if ( (p1Score < 3) || ( p1Score == 3 && p2Score == 3 ) )  //If player 1 is below "40", he scores a point. IF player 1 and player 2 are at "40", player 1 gains advantage.
         {
-            p1.scored();
+            p1Score++;
             System.out.println(p1.getName()+"\t\t\t|\t\t\t"+p2.getName());
-            System.out.println(p1.getScore()+"\t\t|\t\t\t"+p2.getScore());
+            System.out.println(this.getScore(p1Score)+"\t\t|\t\t\t"+this.getScore(p2Score));
             System.out.println("------------------------------------------------------");
         }
-        else if ( (p2.scorePos() < 3) || (p1.scorePos() == 4) ) //Or else (that means player 1 is at "40" or has an advantage), if player 2 is below "40" or player 1 already has an advantage, player 1 wins the game.
+        else if ( (p2Score < 3) || (p1Score == 4) ) //Or else (that means player 1 is at "40" or has an advantage), if player 2 is below "40" or player 1 already has an advantage, player 1 wins the game.
         {
             System.out.println(p1.getName()+" won the game ! Congratulations !");
             System.exit(0);
         }
         else    //The last case is if player 1 is at "40" and player 2 has an advantage. Then player 2 looses his advantage.
         {
-            p2.unScore();
+            p2Score--;
             System.out.println(p1.getName()+"\t\t\t|\t\t\t"+p2.getName());
-            System.out.println(p1.getScore()+"\t\t|\t\t\t"+p2.getScore());
+            System.out.println(this.getScore(p1Score)+"\t\t|\t\t\t"+this.getScore(p2Score));
             System.out.println("------------------------------------------------------");
         }
             
@@ -51,23 +74,23 @@ public class Game {
     //player 2 scored a point
     public void p2Scored(){
         
-        if ( (p2.scorePos() < 3) || ( p1.scorePos() == 3 && p2.scorePos() == 3 ) )  //If player 2 is below "40", he scores a point. IF player 1 and player 2 are at "40", player 1 gains advantage.
+        if ( (p2Score < 3) || ( p1Score == 3 && p2Score == 3 ) )  //If player 2 is below "40", he scores a point. IF player 1 and player 2 are at "40", player 1 gains advantage.
         {
-            p2.scored();
+            p2Score++;
             System.out.println(p1.getName()+"\t\t\t|\t\t\t"+p2.getName());
-            System.out.println(p1.getScore()+"\t\t|\t\t\t"+p2.getScore());
+            System.out.println(this.getScore(p1Score)+"\t\t|\t\t\t"+this.getScore(p2Score));
             System.out.println("------------------------------------------------------");
         }
-        else if ( (p1.scorePos() < 3) || (p2.scorePos() == 4) ) //Or else (that means player 2 is at "40" or has an advantage), if player 1 is below "40" or player 2 already has an advantage, player 2 wins the game.
+        else if ( (p1Score < 3) || (p2Score == 4) ) //Or else (that means player 2 is at "40" or has an advantage), if player 1 is below "40" or player 2 already has an advantage, player 2 wins the game.
         {
             System.out.println(p2.getName()+" won the game ! Congratulations !");
             System.exit(0);
         }
         else    //The last case is if player 2 is at "40" and player 1 has an advantage. Then player 1 looses his advantage.
         {
-            p1.unScore();
+            p1Score--;
             System.out.println(p1.getName()+"\t\t\t|\t\t\t"+p2.getName());
-            System.out.println(p1.getScore()+"\t\t|\t\t\t"+p2.getScore());
+            System.out.println(this.getScore(p1Score)+"\t\t|\t\t\t"+this.getScore(p2Score));
             System.out.println("------------------------------------------------------");
         }
         
